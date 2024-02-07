67dc0460a68ecea2b6e297236cdf2463fa923508
HumanEntity <human_entity@icloud.com>
2024-02-07 20:07:01 +0100

    Add no module implementation of create_asset

--- src/lib.rs
+++ src/lib.rs
@@ -118,6 +118,117 @@ macro_rules! create_asset {
             }
         }
     };
+    (
+        $($derive:ident,)*;
+        $asset_name:ident
+        $(
+            $field_name:ident : $field_type: ty $( = $handle_path:ident)?,
+        )*;
+        $asset_plugin:ident,
+        $asset_loader:ident,
+        $extensions:expr,
+    ) => {
+        use bevy::prelude::*;
+        use bevy::asset::ReflectAsset;
+        #[allow(unused_imports)]
+        use super::*;
+        #[derive(serde::Deserialize, bevy::asset::Asset, bevy::prelude::Reflect)]
+        #[cfg_attr(feature = "saver", derive(serde::Serialize))]
+        #[derive($($derive ,)*)]
+        #[reflect(Asset)]
+        pub struct $asset_name {
+            $(
+                $(
+                    pub $handle_path: String,
+                    #[serde(skip)]
+                )?
+                pub $field_name : $field_type,
+            )*
+        }
+
+        pub struct $asset_plugin;
+        impl bevy::prelude::Plugin for $asset_plugin {
+            fn build(&self, app: &mut bevy::prelude::App) {
+                app.register_type::<$asset_name>()
+                    .register_asset_reflect::<$asset_name>()
+                    .init_asset::<$asset_name>()
+                    .init_asset_loader::<$asset_loader>()
+                    .add_systems(bevy::prelude::Update, finalize_asset);
+            }
+        }
+
+        #[allow(unused_variables)]
+        #[allow(unused_mut)]
+        fn finalize_asset(
+            mut asset_events: EventReader<AssetEvent<$asset_name>>,
+            mut assets: ResMut<Assets<$asset_name>>,
+            asset_server: Res<bevy::prelude::AssetServer>,
+            ) {
+            for event in asset_events.read() {
+                match event {
+                    AssetEvent::Added { id } => {
+                        if let Some(asset) = assets.get_mut(*id) {
+                            $(
+                                $(
+                                    asset.$field_name = asset_server.load(&asset.$handle_path);
+                                )?
+                            )*
+                        }
+                    }
+                    _ => ()
+                }
+            }
+        }
+
+        #[derive(Default)]
+        pub struct $asset_loader;
+        impl bevy::asset::AssetLoader for $asset_loader {
+            type Asset = $asset_name;
+            type Settings = ();
+            type Error = ron::de::Error;
+            fn load<'a>(
+                    &'a self,
+                    reader: &'a mut bevy::asset::io::Reader,
+                    _settings: &'a Self::Settings,
+                    _load_context: &'a mut bevy::asset::LoadContext,
+                ) -> bevy::utils::BoxedFuture<'a, Result<Self::Asset, Self::Error>> {
+                use bevy::asset::AsyncReadExt;
+                Box::pin(async move {
+                    let mut bytes = Vec::new();
+                    reader.read_to_end(&mut bytes).await?;
+                    let custom_asset = ron::de::from_bytes::<$asset_name>(&bytes)?;
+                    Ok(custom_asset)
+                })
+            }
+
+            fn extensions(&self) -> &[&str] {
+                $extensions
+            }
+        }
+        #[cfg(feature = "saver")]
+        impl bevy::asset::saver::AssetSaver for $asset_loader {
+            type Asset = $asset_name;
+            type Settings = ();
+            type Error = ron::Error;
+            type OutputLoader = Self;
+            fn save<'a>(
+                &'a self,
+                writer: &'a mut bevy::asset::io::Writer,
+                asset: bevy::asset::saver::SavedAsset<'a, Self::Asset>,
+                _settings: &'a Self::Settings
+            ) -> core::pin::Pin<Box<dyn core::future::Future<Output = Result<<Self::OutputLoader as bevy::asset::AssetLoader>::Settings, Self::Error>> + Send + 'a>> {
+                use bevy::asset::AsyncWriteExt;
+                Box::pin(async move {
+                    let asset = asset.get();
+                    let string = ron::ser::to_string_pretty(asset, ron::ser::PrettyConfig::default())?;
+                    let bytes = string.as_bytes();
+
+                    writer.write(&bytes).await?;
+                    Ok(())
+                })
+            }
+        }
+    }
 }
 
 #[cfg(test)]
